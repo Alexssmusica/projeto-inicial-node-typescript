5f9a8f382c2ef0c87b3234352e27d1b558ea61a0
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2022-11-15 00:19:52 -0300

    fix: ajustes

--- src/infra/controller/AppController.ts
+++ src/infra/controller/AppController.ts
@@ -2,11 +2,10 @@ import HttpServer from '../http/HttpServer';
 
 export default class AppController {
 	constructor(readonly httpServer: HttpServer) {
-		httpServer.route('get', '/', function () {
-			const output = {
+		httpServer.route('get', '/', async function () {
+			return {
 				message: 'Servidor iniciado.'
 			};
-			return output;
 		});
 	}
 }
