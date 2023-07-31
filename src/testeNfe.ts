8dc00352439687bbdec1a5b766d917e9cebe7c61
Alex Santos de Souza <alexssmusica@gmail.com>
2023-07-31 16:38:35 -0300

    update

--- src/testeNfe.ts
+++ src/testeNfe.ts
@@ -5,12 +5,15 @@ import fs from 'fs/promises';
 import path from 'path';
 
 async function init() {
-	const caminho = path.resolve('41230605333353000127651090000010869709527181-procNFe.xml');
+	console.log('Starting');
+	const caminho = path.resolve('41230705333353000127551200000000281466014924-procNFe.xml');
+	console.log(caminho);
 
 	const arquivo = await fs.readFile(caminho, 'utf8');
+	console.log(arquivo);
 	const pdf = await gerarPDF(arquivo);
 	pdf.pipe(createWriteStream('out.pdf'));
-	console.log(pdf);
+	// console.log(pdf);
 }
 
 init();
