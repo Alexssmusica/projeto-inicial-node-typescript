ea19099dab59f9824e21c8051a629a082b8bca40
Alex Santos de Souza <alexssmusica@gmail.com>
2023-09-27 09:06:06 -0300

    update testes

--- src/testeNfe.ts
+++ src/testeNfe.ts
@@ -1,4 +1,4 @@
-import { gerarPDF } from 'node-nfe-nfce';
+import { gerarPDF } from '@alexssmusica/node-pdf-nfe';
 
 import { createWriteStream } from 'fs';
 import fs from 'fs/promises';
@@ -6,11 +6,12 @@ import path from 'path';
 
 async function init() {
 	console.log('Starting');
-	const caminho = path.resolve('41230705333353000127551200000000281466014924-procNFe.xml');
+	const caminho = path.resolve('41230905333353000127551090000002861418075739-procNFe.xml');
+	const logo = path.resolve('logo.png');
 
 	const arquivo = await fs.readFile(caminho, 'utf8');
 
-	const pdf = await gerarPDF(arquivo);
+	const pdf = await gerarPDF(arquivo, logo);
 	pdf.pipe(createWriteStream('out.pdf'));
 	// console.log(pdf);
 }
