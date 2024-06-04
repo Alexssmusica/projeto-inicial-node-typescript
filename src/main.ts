e4dfdf0600425133722d5556d38aa7071ba6f15d
Alex Santos de Souza <alexssmusica@gmail.com>
2024-06-03 23:54:01 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,24 @@ import AppController from './infra/controller/AppController';
 import ExpressAdapter from './infra/http/ExpressAdapter';
 import Env from './infra/util/Env';
 
-const http = new ExpressAdapter();
+import cluster from 'cluster';
+import { cpus } from 'os';
+const numOfCPUs = cpus().length;
+if (cluster.isPrimary) {
+	console.log(`Processo principal criado: ${process.pid}`);
 
-new AppController(http);
+	for (let i = 0; i < numOfCPUs; i++) {
+		console.log(`Criando processo filho: ${i}...`);
+		cluster.fork();
+	}
+	cluster.on('exit', (worker, code, signal) => {
+		console.log(`Processo ${worker.process.pid} finalizado`);
+		cluster.fork();
+	});
+} else {
+	const http = new ExpressAdapter();
 
-http.listen(Env.variable.PORT);
+	new AppController(http);
+
+	http.listen(Env.variable.PORT);
+}
