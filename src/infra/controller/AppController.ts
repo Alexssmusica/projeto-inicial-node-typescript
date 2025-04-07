d14a86ff001437329ffe8b8b9ca7cfd667e6dff5
Alex Santos de Souza <alexssmusica@gmail.com>
2025-04-07 11:00:30 -0300

    service

--- src/infra/controller/AppController.ts
+++ src/infra/controller/AppController.ts
@@ -1,10 +1,12 @@
+import AppService from '../../service/AppService';
 import type HttpServer from '../http/HttpServer';
 
 export default class AppController {
 	constructor(readonly httpServer: HttpServer) {
 		httpServer.route('get', '/', () => {
+			const appService = new AppService();
 			return {
-				message: 'Servidor iniciado.'
+				message: appService.execute()
 			};
 		});
 	}
---
+++ src/service/AppService.ts
@@ -0,0 +1,7 @@
+export default class AppService {
+	constructor() {}
+
+	execute() {
+		return 'Servidor iniciado.';
+	}
+}
