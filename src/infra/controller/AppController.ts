8b1def6ceaeac4b995e344f4a08f23fde70a482c
Alex Santos de Souza <alexssmusica@gmail.com>
2025-04-07 10:47:07 -0300

    Update AppController.ts

--- src/infra/controller/AppController.ts
+++ src/infra/controller/AppController.ts
@@ -2,7 +2,7 @@ import type HttpServer from '../http/HttpServer';
 
 export default class AppController {
 	constructor(readonly httpServer: HttpServer) {
-		httpServer.route('get', '/', function () {
+		httpServer.route('get', '/', () => {
 			return {
 				message: 'Servidor iniciado.'
 			};
